71a3c4d6a356206384516f32737a5edd4f51460a
Kiramman <kiramman89@gmail.com>
2023-08-05 19:06:09 +0700

    Fix

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
-            public int calculate (int weightKg, double heightM) {
-                return (int) (weightKg / (heightM * heightM));
+    public int calculate(int weightKg, double heightM) {
+        return (int) (weightKg / (heightM * heightM));
 
-        }
+    }
 }
